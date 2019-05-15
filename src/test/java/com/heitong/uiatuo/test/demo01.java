a69208e083bbcf0b8cf6214b8a249b1dc40b0c9e
mrye111 <2316725415@qq.com>
2019-05-15 14:12:22 +0800

    test

--- src/test/java/com/heitong/uiatuo/test/demo01.java
+++ src/test/java/com/heitong/uiatuo/test/demo01.java
@@ -1,6 +1,9 @@
 package com.heitong.uiatuo.test;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeTest;
@@ -10,19 +13,17 @@ import org.testng.annotations.Test;
 import java.net.URL;
 
 public class demo01 {
-    static  DesiredCapabilities dc;
+    static  WebDriver driver;
     @Test(dataProvider = "driver")
     public  static  void Test(String drivers) throws  Exception{
-        if (drivers.equals("chrome")){
-             dc= DesiredCapabilities.chrome();
-        }else if (drivers.equals("ie")){
-            dc= DesiredCapabilities.internetExplorer();
-        }else if (drivers.equals("firfox")){
-            dc= DesiredCapabilities.firefox();
-        }
-
-        WebDriver driver=new RemoteWebDriver(new URL("http://192.168.10.6:4444/wd/hub"),dc);
-
+        if (drivers.equals("chrome"))
+            driver=new ChromeDriver();
+        else  if (drivers.equals("ie"))
+            driver=new FirefoxDriver();
+        else  if (drivers.equals("firfox"))
+            driver=new InternetExplorerDriver();
+        else
+            System.out.println("error");
         driver.get("https://www.baidu.com");
     }
 
